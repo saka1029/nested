2f51a7a69663e68717b7fb756fe4dcd7582a7b8a
saka1029 <saka1029@gmail.com>
2026-03-11 17:29:46 +0900

    ..

---
+++ src/test/java/saka1029/nested/TestRegex.java
@@ -0,0 +1,22 @@
+package saka1029.nested;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class TestRegex {
+
+    @Test
+    public void testB() {
+        Pattern word = Pattern.compile("^if\\b");
+        assertTrue(word.matcher("if ").find());
+        assertTrue(word.matcher("if-").find());
+        assertFalse(word.matcher("iff").find());
+        assertFalse(word.matcher("if3").find());
+        assertFalse(word.matcher("if_").find());
+    }
+
+}

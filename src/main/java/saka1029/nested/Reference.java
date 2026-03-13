2e1f1e42268d0b3a5a25fd84fd83d513fdde4299
saka1029 <saka1029@gmail.com>
2026-03-14 07:17:58 +0900

    ..

--- src/main/java/saka1029/nested/Reference.java
+++ src/main/java/saka1029/nested/Reference.java
@@ -2,6 +2,14 @@ package saka1029.nested;
 
 /**
  * グローバル変数、ローカル変数、ルーチン参照
+ * <pre>
+ *   Reference
+ *      △
+ *      |
+ *      +------+-------------------+
+ *      |      |      variables    |
+ *   Global  Local <*----------- Routine
+ * </pre>
  */
 public class Reference {
 
